46d66e3d231e09724c68bc75f1c23060d0768f6e
DiachenkoD <dimaecio1@gmail.com>
2017-02-05 20:35:55 +0200

    AddSomeChangesFinally

---
+++ src/BugGame/BugGame/Button.h
@@ -0,0 +1,56 @@
+////////////////////////////////////
+// Cockroach
+//
+// Author: Eugene
+//
+// 29.01.17
+////////////////////////////////////
+
+#ifndef _Button_h_
+#define _Button_h_
+
+////////////////////////////////////
+// Includes
+#include "GameObject.h"
+#include "Levels.h"
+
+////////////////////////////////////
+PTR(Button);
+
+
+
+class Button
+	: public GameObject
+{
+public:
+
+	////////////////////////////////////
+	Button();
+
+	////////////////////////////////////
+	virtual void update(F32 dt) OVERRIDE;
+
+	////////////////////////////////////
+	virtual void render(sf::RenderWindow* rw) OVERRIDE;
+
+	///////////////////////////////////
+	inline const sf::Mouse* getMouseButton() const { return m_mouse; }
+
+	////////////////////////////////////
+	void setPosition( const F32& x, const F32& y ){ }
+
+	////////////////////////////////////
+	static ButtonPtr Create();
+
+private:
+
+	////////////////////////////////////
+	bool init();
+
+protected:
+
+	sf::Mouse* m_mouse;
+
+};
+
+#endif
